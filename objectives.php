95dfa2e116c208d8a33fb07d7d5172cb07d36eb5
Wanga Kanjala <75719614+V014@users.noreply.github.com>
2023-04-27 20:56:38 +0200

    Updated objectives page

--- objectives.php
+++ objectives.php
@@ -129,20 +129,24 @@
                     </div>
                 </nav>
                 <div class="container-fluid">
-                    <h3 class="text-dark mb-4">Clients</h3>
-                    <div class="card shadow">
+                    <h3 class="text-dark mb-4">Objectives</h3>
+                    <div class="card shadow mb-4">
                         <div class="card-header py-3">
-                            <p class="text-primary m-0 fw-bold">Client Information</p>
+                            <p class="text-primary m-0 fw-bold">Objective Information</p>
                         </div>
                         <div class="card-body">
                             <div class="row">
                                 <div class="col-md-6 text-nowrap">
-                                    <div id="dataTable_length" class="dataTables_length" aria-controls="dataTable"><label class="form-label">Show&nbsp;<select class="d-inline-block form-select form-select-sm">
+                                    <div id="dataTable_length" class="dataTables_length" aria-controls="dataTable">
+                                        <label class="form-label">Show&nbsp;
+                                            <select class="d-inline-block form-select form-select-sm">
                                                 <option value="10" selected="">10</option>
                                                 <option value="25">25</option>
                                                 <option value="50">50</option>
                                                 <option value="100">100</option>
-                                            </select>&nbsp;</label></div>
+                                            </select>&nbsp;
+                                        </label>
+                                    </div>
                                 </div>
                                 <div class="col-md-6">
                                     <div class="text-md-end dataTables_filter" id="dataTable_filter"><label class="form-label"><input type="search" class="form-control form-control-sm" aria-controls="dataTable" placeholder="Search"></label></div>
@@ -202,6 +206,52 @@
                             </div>
                         </div>
                     </div>
+                    <div class="card shadow">
+                        <div class="card-header py-3">
+                            <p class="text-primary m-0 fw-bold">Set Objectives</p>
+                        </div>
+                        <div class="card-body">
+                            <div class="table-responsive table mt-2" id="dataTable" role="grid" aria-describedby="dataTable_info">
+                                <table class="table my-0" id="dataTable">
+                                    <thead>
+                                        <tr>
+                                            <th>SquadID</th>
+                                            <th>Title</th>
+                                            <th>Activity</th>
+                                            <th>From Time</th>
+                                            <th>To Time</th>
+                                            <th>Location</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>
+                                                <select class="d-inline-block form-select form-select-sm">
+                                                    <option value="1" selected="">1</option>
+                                                    <option value="2">2</option>
+                                                </select>&nbsp;
+                                            </td>
+                                            <td><input class="form-control form-control-sm" type="text" id="title" placeholder="Title" name="title"></td>
+                                            <td><input class="form-control form-control-sm" type="text" id="activity" placeholder="Activity" name="activity"></td>
+                                            <td><input class="form-control form-control-sm" type="text" id="fromtime" placeholder="Form Time" name="fromtime"></td>
+                                            <td><input class="form-control form-control-sm" type="text" id="totime" placeholder="To Time" name="totime"></td>
+                                            <td><input class="form-control form-control-sm" type="text" id="location" placeholder="Location" name="location"></td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-6 align-self-center">
+                                    <p id="dataTable_info" class="dataTables_info" role="status" aria-live="polite">Fill in all fields to set an objective</p>
+                                </div>
+                                <div class="col-md-6">
+                                    <nav class="d-lg-flex justify-content-lg-end dataTables_paginate paging_simple_numbers">
+                                        <button class="btn btn-primary btn-sm">Set Objective</button>
+                                    </nav>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
             <footer class="bg-white sticky-footer">
@@ -209,7 +259,8 @@
                     <div class="text-center my-auto copyright"><span>Copyright © Brand 2023</span></div>
                 </div>
             </footer>
-        </div><a class="border rounded d-inline scroll-to-top" href="#page-top"><i class="fas fa-angle-up"></i></a>
+        </div>
+        <a class="border rounded d-inline scroll-to-top" href="#page-top"><i class="fas fa-angle-up"></i></a>
     </div>
     <script src="assets/bootstrap/js/bootstrap.min.js"></script>
     <script src="assets/js/theme.js"></script>
