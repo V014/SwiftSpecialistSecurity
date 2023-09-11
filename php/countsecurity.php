2fbe27b651177b120d61288630c5021ddd1f1034
Wanga Kanjala <75719614+V014@users.noreply.github.com>
2023-09-11 16:41:52 +0200

    Fixed security guard count bug on client dashboard

--- php/countsecurity.php
+++ php/countsecurity.php
@@ -1,13 +1,13 @@
 <?php
   // Retrieve the user ID from the session and store it in a variable for ease of use
   $UserID = $_SESSION['UserID'];
-  $queryClient = "SELECT COUNT(ClientID) FROM client WHERE UserID = $UserID";
+  $queryClient = "SELECT ClientID FROM client WHERE UserID = $UserID";
   $resultClient = mysqli_query($connection, $queryClient);
   $RowClientID = mysqli_fetch_array($resultClient);
   $ClientID = $RowClientID[0];
 
   // Identify the squad which is related to the client
-  $querySquad = "SELECT COUNT(SquadID) FROM squad WHERE ClientID = $ClientID";
+  $querySquad = "SELECT SquadID FROM squad WHERE ClientID = $ClientID";
   $resultSquad = mysqli_query($connection, $querySquad);
   $RowSquadID = mysqli_fetch_array($resultSquad);
   $SquadID = $RowSquadID[0];
