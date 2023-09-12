4f09b243ecf2d256b20ba1bd93dd7ed013d29594
Wanga Kanjala <75719614+V014@users.noreply.github.com>
2023-09-12 15:20:49 +0200

    Revert "tuned payments counter on client dashboard"
    This reverts commit 15412640282515768e19f6bb55e399eb4ef6a97d.

--- php/countclientpayments.php
+++ php/countclientpayments.php
@@ -1,9 +1,17 @@
 <?php
-  // Retrieve the number of Payment from the database
-  $query = "SELECT COUNT(PaymentID) FROM clientpayments";
-  $result = mysqli_query($connection, $query);
-  $row = mysqli_fetch_array($result);
-  $num_payments = $row[0];
+  // Retrieve the user ID from the session and store it in a variable for ease of use
+  $UserID = $_SESSION['UserID'];
+  $queryClient = "SELECT ClientID FROM client WHERE UserID = $UserID";
+  $resultClient = mysqli_query($connection, $queryClient);
+  $RowClientID = mysqli_fetch_array($resultClient);
+  $ClientID = $RowClientID[0];
+
+  // Identify the payments that are related to the client
+  $queryPayments = "SELECT COUNT(PaymentID) FROM clientpayments WHERE ClientID = $ClientID";
+  $resultPayments = mysqli_query($connection, $queryPayments);
+  $RowPaymentsID = mysqli_fetch_array($resultPayments);
+
+  $num_payments = $RowPaymentsID[0];
 
   // Convert the result into JSON format
   $payments_json = json_encode($num_payments);
--- php/countclientreports.php
+++ php/countclientreports.php
@@ -1,6 +1,19 @@
 <?php
+  // Retrieve the user ID from the session and store it in a variable for ease of use
+  $UserID = $_SESSION['UserID'];
+  $queryClient = "SELECT ClientID FROM client WHERE UserID = $UserID";
+  $resultClient = mysqli_query($connection, $queryClient);
+  $RowClientID = mysqli_fetch_array($resultClient);
+  $ClientID = $RowClientID[0];
+
+  // Identify the squad which is related to the client
+  $querySquad = "SELECT SquadID FROM squad WHERE ClientID = $ClientID";
+  $resultSquad = mysqli_query($connection, $querySquad);
+  $RowSquadID = mysqli_fetch_array($resultSquad);
+  $SquadID = $RowSquadID[0];
+  
   // Retrieve the number of clients from the database
-  $query = "SELECT COUNT(ReportID) FROM report";
+  $query = "SELECT COUNT(r.ReportID) FROM Report AS r INNER JOIN objectives As o ON o.ObjectiveID = r.ObjectiveID WHERE o.SquadID = $SquadID";
   $result = mysqli_query($connection, $query);
   $row = mysqli_fetch_array($result);
   $num_report = $row[0];
